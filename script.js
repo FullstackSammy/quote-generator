c1fd97185d34de905cd4cc1d606289580130437e
samzzii <nasstromsamuel@icloud.com>
2022-11-10 17:10:30 +0100

    Add getQuotes Function

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+let apiQuotes = [];
 
+// Show New Quote
+function newQuote() {
+    
+}
+
+
+// Get Quotes From API
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+
+    };
+};
+
+
+// On load
+getQuotes();
