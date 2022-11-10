e348c0a69fdd8540da30608e64f0a7665ec2e670
samzzii <nasstromsamuel@icloud.com>
2022-11-10 17:45:16 +0100

    Add more functionality to newQuote function

--- script.js
+++ script.js
@@ -10,7 +10,18 @@ let apiQuotes = [];
 /** Picks a random quote from apiQuotes array and displays it in the browser*/
 function newQuote() {
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    authorText.textContent = quote.author;
+    // Check if Author field is blank and replace it with "Unknown"
+    if (!quote.author) {
+        authorText.textContent = "Unknown";
+    } else {
+        authorText.textContent = quote.author;
+    };
+    // Check Quote length to determine the styling
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
     quoteText.textContent = quote.text;
 };
 
