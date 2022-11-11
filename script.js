67a88e8373e3df08240bc89aafe506572c3d6260
samzzii <nasstromsamuel@icloud.com>
2022-11-11 12:02:58 +0100

    Add spinning loader

--- script.js
+++ script.js
@@ -4,11 +4,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+/** Shows the user that the page is loading */
+function loading() {
+    loader.hidden = false
+    quoteContainer.hidden = true
+};
+
+/** Hides the loading wheel and dispays the quote when finished loading */
+function complete() {
+    quoteContainer.hidden = false
+    loader.hidden = true
+}
+
 /** Picks a random quote from apiQuotes array and displays it in the browser*/
 function newQuote() {
+    loading();
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // Check if Author field is blank and replace it with "Unknown"
     if (!quote.author) {
@@ -22,12 +36,15 @@ function newQuote() {
     } else {
         quoteText.classList.remove('long-quote');
     }
+    // Set Quote, hide loader
     quoteText.textContent = quote.text;
+    complete();
 };
 
 
 /** Gets Quotes From API */
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
@@ -41,7 +58,7 @@ async function getQuotes() {
 
 /** Tweets Quote */
 function tweetQuote() {
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    const twitterUrl = `https://twitter.com/intent/tweet?text="${quoteText.textContent}" - ${authorText.textContent}`;
     window.open(twitterUrl, '_blank');
 }
 
